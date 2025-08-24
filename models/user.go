22cae9010f2db45ab06fd4db967ab9372d0bb279
laiszig <laisziegler@gmail.com>
2025-08-24 11:17:31 -0300

    implement login logic

--- models/user.go
+++ models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "event-booking/db"
+import (
+	"errors"
+	"event-booking/db"
+	"event-booking/utils"
+)
 
 type User struct {
 	ID       int64
@@ -17,7 +21,12 @@ func (u User) Save() error {
 
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 	if err != nil {
 		return err
 	}
@@ -26,4 +35,21 @@ func (u User) Save() error {
 
 	u.ID = userId
 	return err
+}
+
+func (u User) ValidateCredentials() error {
+	query := "SELECT password FROM users WHERE email = ?"
+	row := db.DB.QueryRow(query, u.Email)
+	var retrievedPassword string
+	err := row.Scan(&retrievedPassword)
+	if err != nil {
+		return errors.New("credentials invalid")
+	}
+
+	passwordIsValid := utils.CheckPasswordHash(u.Password, retrievedPassword)
+	if !passwordIsValid {
+		return errors.New("credentials invalid")
+	}
+
+	return nil
 }
--- routes/users.go
+++ routes/users.go
@@ -9,7 +9,6 @@ import (
 
 func signUpUser(ctx *gin.Context) {
 	var user models.User
-
 	err := ctx.ShouldBindJSON(&user)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
@@ -25,4 +24,23 @@ func signUpUser(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{"message": "User created successfully."})
+}
+
+func userLogin(ctx *gin.Context) {
+	var user models.User
+	err := ctx.ShouldBindJSON(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "Failed to parse request data."})
+		return
+	}
+
+	err = user.ValidateCredentials()
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{
+			"message": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "User login successfully."})
 }
